74ff9c8efa641c4e0ce2cb34f8cad28143ff38fa
M.R.I <mri08062000@gmail.com>
2024-08-11 14:40:03 +0700

    minor fixes

--- src/main/java/com/customer/realtime/repository/CustomerRepository.java
+++ src/main/java/com/customer/realtime/repository/CustomerRepository.java
@@ -1,13 +1,10 @@
 package com.customer.realtime.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.customer.realtime.model.Customer;
 import java.util.Optional;
 
-
-@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long>{
     Optional<Customer> findByCustomerName(String customerName);
 }
